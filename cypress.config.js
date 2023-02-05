cd00b87546c4ebac4d10d142913a253894dc9762
Benjamin Heiskell <ben@xdxa.org>
2023-02-05 15:48:47 -0500

    Fix Cypress test and execute redirect

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://localhost:30000',
   },
 });
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,6 @@
 describe("empty spec", () => {
    it("passes", () => {
      cy.visit("/");
+     cy.url().should('be.equal', `${Cypress.config("baseUrl")}/license`);
    });
  });
