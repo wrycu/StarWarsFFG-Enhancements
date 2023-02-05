3c339e89f9b116e5359442084d0357d1c9a77afa
Benjamin Heiskell <ben@xdxa.org>
2023-02-05 18:03:43 -0500

    Add default viewport configuration and wait to verify initial page load in CI

--- cypress.config.js
+++ cypress.config.js
@@ -4,4 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:30000',
   },
+  viewportWidth: 1920,
+  viewportHeight: 1080,
 });
--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,6 +1,8 @@
-describe("empty spec", () => {
-   it("passes", () => {
+describe.only("empty spec", () => {
+   it.only("passes", () => {
      cy.visit("/");
      cy.url().should('be.equal', `${Cypress.config("baseUrl")}/license`);
+     // Temporary wait statement to make sure license page actually loads
+     cy.wait(1000);
    });
  });
