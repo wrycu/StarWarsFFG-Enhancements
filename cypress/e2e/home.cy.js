a0f31a79c96bc51487a5af6b2afa0d79875e6789
Benjamin Heiskell <ben@xdxa.org>
2023-02-07 23:47:10 -0500

    Continuing automation of initial installation
    This is the general stubbing, with selectors that would likely benefit from
    refinement. However, I want this to be idempotent, which it is not currently.

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -31,10 +31,21 @@ describe.only("initialization", () => {
             cy.get('#notifications > .notification').contains('installed successfully', {timeout: 10000});
             // Close dialog
             cy.get('.header-button').click();
-
-            cy.get('.sheet-tabs > [data-tab="worlds"]').click();
           }
         });
+
+        // Navigate to install modules
+        cy.get('.sheet-tabs > [data-tab="modules"]').click();
+
+        // Cilck Install modules
+        cy.get('.active > .setup-footer > .install-package').click();
+
+        // Install fxmaster
+        cy.get('[data-package-id="fxmaster"] > .package-controls > .install').click();
+        cy.get('[data-package-id="lib-wrapper"] > .package-controls > .install').click();
+        cy.get('[data-package-id="statuscounter"] > .package-controls > .install').click();
+
+        cy.get('.sheet-tabs > [data-tab="worlds"]').click();
 
         // Open create world dialog
         cy.get('#create-world > label').click();
@@ -46,13 +57,12 @@ describe.only("initialization", () => {
         cy.get('form > :nth-child(3) > .form-fields > select').select('Star Wars FFG');
 
         // Create the world
-        cy.get('[type="submit"]').click();
+        cy.get('form > [type="submit"]').click();
 
         // Launch the world
         cy.get('.package-controls > [name="action"]').click();
       }
     });
-
 
     cy.visit(`${Cypress.config("baseUrl")}/join`);
     cy.url().should('eq', `${Cypress.config("baseUrl")}/join`);
@@ -64,5 +74,27 @@ describe.only("initialization", () => {
     cy.get(':nth-child(1) > button').click();
 
     cy.url().should('eq', `${Cypress.config("baseUrl")}/game`);
+
+    // Clear welcome message if it exists
+    cy.get('.step-header > .step-button > .far').click();
+
+    // Close warning
+    cy.get('.dialog-button').click();
+
+    // Close all notifications
+    //cy.get('#notifications .close').click({ multiple: true })
+
+    // Activate modules
+    cy.get('[data-tab="settings"] > .fas').click();
+
+    cy.get('[data-action="modules"]').click();
+
+    cy.get('.package-overview > .package-title > .active').click({multiple: true});
+
+    cy.get('footer.flexrow > [type="submit"]').click();
+
+    cy.get('.window-content > .dialog-buttons > .yes').click({multiple: true});
+
+    cy.get('[data-control="ffg-star-wars-enhancements"] > .fa').click();
   });
 });
