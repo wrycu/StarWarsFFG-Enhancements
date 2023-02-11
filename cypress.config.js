66752526d58a50641aecaac6043d2e3414ac2b8b
Benjamin Heiskell <ben@xdxa.org>
2023-02-10 23:48:07 -0500

    Source cypress URL from the environment
    Unsetting the default to remove the risk of accidentally running it against a real environment.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:30000',
+    baseUrl: 'http://example:30000', // Override with CYPRESS_BASE_URL
   },
   viewportWidth: 1920,
   viewportHeight: 1080,
