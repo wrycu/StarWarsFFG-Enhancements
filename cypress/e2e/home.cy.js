bd6a5178bb294788911c09b9e9c5cac76d1fcc35
Benjamin Heiskell <ben@xdxa.org>
2023-02-11 23:44:30 -0500

    Try to force some clicks

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -234,8 +234,8 @@ describe.only("ffg-star-wars-enhancements", () => {
     });
 
     // Open the crawl dialog
-    cy.get('[data-control="ffg-star-wars-enhancements"]').click();
-    cy.get('[data-tool="opening-crawl"]').click();
+    cy.get('[data-control="ffg-star-wars-enhancements"]').click({force: true});
+    cy.get('[data-tool="opening-crawl"]').click({force: true});
 
     // Create a folder for the Opening Crawl journals
     cy.get('.window-content .yes').click();
@@ -246,8 +246,8 @@ describe.only("ffg-star-wars-enhancements", () => {
     // Can't seem to close the journal at this point, so just minimize it to keep it out of view
     cy.get('.journal-entry > .window-header > .close').dblclick();
 
-    cy.get('[data-control="ffg-star-wars-enhancements"]').click();
-    cy.get('[data-tool="opening-crawl"]').click();
+    cy.get('[data-control="ffg-star-wars-enhancements"]').click({force: true});
+    cy.get('[data-tool="opening-crawl"]').click({force: true});
 
     // Launch the opening crawl
     cy.get('#ffg-star-wars-enhancements-opening-crawl-select form button[type="submit"]').each(($button) => {
