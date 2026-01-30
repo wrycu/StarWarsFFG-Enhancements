d618316c9fd044f4e7c81165ec7929a2ec93bc9f
YeNov <novakov.eo@gmail.com>
2026-01-30 21:01:14 +0100

    Update to enhancements tools to make it work in v13
    Changed how tools are structured to meet the new standards (probably). Now it opens up on click. Each tool functionality is not checked yet

--- scripts/controls_layer.js
+++ scripts/controls_layer.js
@@ -16,79 +16,94 @@ export const register_controls = (controls) => {
         layer: "ffgenhancements",
         icon: "fa fa-jedi",
         visible: game.user.isGM,
-        tools: {
-            crawl: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.controls.opening-crawl.name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.controls.opening-crawl.title"),
-                icon: "fas fa-journal-whills",
-                button: true,
-                onChange: () => {
-                    select_opening_crawl();
-                },
-            },
-            title_cards: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.controls.title-cards.name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.controls.title-cards.title"),
-                icon: "fas fa-book",
-                button: true,
-                onChange: () => {
-                    title_cards_dialog();
-                },
-            },
-            datapad_journal: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.controls.new-journal-template.name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.controls.new-journal-template.title"),
-                icon: "fas fa-book-medical",
-                button: true,
-                onChange: () => {
-                    create_datapad_journal();
-                },
-            },
-            shop_tool: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.shop.html.scene.name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.shop.html.scene.title"),
-                icon: "fas fa-shopping-cart",
-                button: true,
-                onChange: () => {
-                    shop_creator();
-                },
-            },
-            hyperspace: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.controls.hyperspace.name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.controls.hyperspace.title"),
-                icon: "fas fa-rocket",
-                button: true,
-                onChange: () => {
-                    select_hyperspace();
-                },
-            },
-            attack_animations: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.attack-animation.custom.button-name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.attack-animation.custom.button-title"),
-                icon: "fas fa-bullseye",
-                button: true,
-                onChange: async () => {
-                    await configure_attack_animation();
-                },
-            },
-            custom_attack_animations: {
-                name: game.i18n.localize("ffg-star-wars-enhancements.attack-animation.global-custom.button-name"),
-                title: game.i18n.localize("ffg-star-wars-enhancements.attack-animation.global-custom.button-title"),
-                icon: "fa-duotone fa-bow-arrow",
-                button: true,
-                onChange: async () => {
-                    await configure_custom_attack_animation();
-                },
-            }
+        tools: {},
+        activeTool: "",
+        onChange: (event, active) => {
+            // maybe need to do here something, idk
         }
     };
-
+    
+    // init enhancements controls tools
+    {
+        let crawlName = game.i18n.localize("ffg-star-wars-enhancements.controls.opening-crawl.name");
+        ffg_sw_enhancements_controls.tools[crawlName] = {
+            name: crawlName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.controls.opening-crawl.title"),
+            icon: "fas fa-journal-whills",
+            button: true,
+            onChange: () => {
+                select_opening_crawl();
+            }
+        };
+        ffg_sw_enhancements_controls.activeTool = crawlName;
+        let titleCardsName = game.i18n.localize("ffg-star-wars-enhancements.controls.title-cards.name");
+        ffg_sw_enhancements_controls.tools[titleCardsName] = {
+            name: titleCardsName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.controls.title-cards.title"),
+            icon: "fas fa-book",
+            button: true,
+            onChange: () => {
+                title_cards_dialog();
+            }
+        };
+        let datapadJournalName = game.i18n.localize("ffg-star-wars-enhancements.controls.new-journal-template.name");
+        ffg_sw_enhancements_controls.tools[datapadJournalName] = {
+            name: datapadJournalName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.controls.new-journal-template.title"),
+            icon: "fas fa-book-medical",
+            button: true,
+            onChange: () => {
+                create_datapad_journal();
+            }
+        };
+        let shopToolName = game.i18n.localize("ffg-star-wars-enhancements.shop.html.scene.name");
+        ffg_sw_enhancements_controls.tools[shopToolName] = {
+            name: shopToolName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.shop.html.scene.title"),
+            icon: "fas fa-shopping-cart",
+            button: true,
+            onChange: () => {
+                shop_creator();
+            }
+        };
+        let hyperspaceToolName = game.i18n.localize("ffg-star-wars-enhancements.controls.hyperspace.name");
+        ffg_sw_enhancements_controls.tools[hyperspaceToolName] = {
+            name: hyperspaceToolName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.controls.hyperspace.title"),
+            icon: "fas fa-rocket",
+            button: true,
+            onChange: () => {
+                select_hyperspace();
+            },
+        };
+        let attackAnimationsToolName = game.i18n.localize("ffg-star-wars-enhancements.attack-animation.custom.button-name");
+        ffg_sw_enhancements_controls.tools[attackAnimationsToolName] = {
+            name: attackAnimationsToolName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.attack-animation.custom.button-title"),
+            icon: "fas fa-bullseye",
+            button: true,
+            onChange: async () => {
+                await configure_attack_animation();
+            }
+        };
+        let customAttackAnimationsToolName = game.i18n.localize("ffg-star-wars-enhancements.attack-animation.global-custom.button-name");
+        ffg_sw_enhancements_controls.tools[customAttackAnimationsToolName] = {
+            name: customAttackAnimationsToolName,
+            title: game.i18n.localize("ffg-star-wars-enhancements.attack-animation.global-custom.button-title"),
+            icon: "fa-duotone fa-bow-arrow",
+            button: true,
+            onChange: async () => {
+                await configure_custom_attack_animation();
+            },
+        };
+    }
     // grab the existing scene controls
     // update the built-in token section
     let additional_token_controls = controls.tokens;
     // add the hologram button
-    additional_token_controls.tools["Convert to Hologram"] = {
-        name: game.i18n.localize("ffg-star-wars-enhancements.controls.holo.name"),
+    let holo_name = game.i18n.localize("ffg-star-wars-enhancements.controls.holo.name");
+    additional_token_controls.tools[holo_name] = {
+        name: holo_name,
         title: game.i18n.localize("ffg-star-wars-enhancements.controls.holo.title"),
         icon: "fas fa-globe",
         button: true,
@@ -98,5 +113,6 @@ export const register_controls = (controls) => {
     };
 
     // create the enhancements control section
-    controls["ffg-star-wars-enhancement-controls"] = ffg_sw_enhancements_controls;
+    let ffg_enhancements_name = game.i18n.localize("ffg-star-wars-enhancements.controls.name");
+    controls[ffg_enhancements_name] = ffg_sw_enhancements_controls;
 };
